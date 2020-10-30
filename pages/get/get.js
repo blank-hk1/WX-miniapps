ad778aea889577cef1cadbe1bc92ff1e0bbe67cc
blank-hk1 <1577279487@qq.com>
2020-10-30 14:21:16 +0800

    Update get.js

--- pages/get/get.js
+++ pages/get/get.js
@@ -8,7 +8,9 @@ Page({
     getPngSecond: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602692242325&di=c3cbad90b6a86eba9f22b3391bc63fdb&imgtype=0&src=http%3A%2F%2Fdpic.tiankong.com%2Fcp%2Fmh%2FQJ6226751079.jpg",//海星
     getPngThrid: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1699162582,1738986241&fm=26&gp=0.jpg",//漂流瓶 
     dl1:"成功捡到漂流瓶",
-    dl2:"未能成功捡到漂流瓶"
+    dl2:"未能成功捡到漂流瓶",
+    content:"请点击下方按钮",
+    content1:""
   },
   onLoad: function () {
     var _this = this;
@@ -21,40 +23,44 @@ Page({
   },
   getAnswer: function (){
     var num = Math.round(Math.random() * 9 + 1);
-    console.log(num);
+    var number;
+    var that =this;
+    //console.log(num);
+    const db = wx.cloud.database({});
+    const cont = db.collection('Floating');
     if(num>5){
-      /**wx.showModal({
-        title: '提示',
-        content: '未捡到漂流瓶',
-        showCancel: false,
-        success: function (res) {
-          if (res.confirm) {
-            console.log('用户点击确定')
-          }
-        }
-      })
-      return**/
-      this.setData({
-        bgPng: this.data.getPngThrid,
-        dl:this.data.dl1
-      })
+      app.globalData.get=app.globalData.get+1,
+    db.collection("Floating").count()
+    .then(async res=>{
+    let totals = res.total;
+    number= Math.round(Math.random() * totals + 1);
+    //console.log(number)
+    db.collection("Floating").where({
+      数据id:number,
+    }).get({
+      success: function (res) {
+        //console.log(res.data[0].数据内容)
+        
+        that.setData({
+          content:res.data[0].数据内容
+        })            
+      }
+    })
+    })
+    console.log(that.data.content)
+    this.setData({
+      bgPng: this.data.getPngThrid,
+      dl:this.data.dl1,
+      content1:"漂流瓶上的内容是"
+    })  
+    
     }
     else{
-      /**wx.showModal({
-        title: '提示',
-        content: '捡到了漂流瓶',
-        showCancel: false,
-        success: function (res) {
-          if (res.confirm) {
-            console.log('用户点击确定')
-          }
-        }
-      })
-      return**/
-      app.globalData.get=app.globalData.get+1,
       this.setData({
         bgPng: this.data.getPngSecond,
-        dl:this.data.dl2
+        dl:this.data.dl2,
+        content:"",
+        content1:""
       })
     }
   }
