e54d29f58644801cdaf4c56ac301ca221d02f651
blank-hk1 <1577279487@qq.com>
2021-01-11 21:41:36 +0800

    Create custom-progress.js

---
+++ miniprogram-6/miniprogram/components/custom-progress/custom-progress.js
@@ -0,0 +1,14 @@
+Component({
+    properties: {
+        percent:{
+            type: Number,
+            value: 0
+        }
+    },
+    data: {},
+    methods: {},
+    options: {
+        styleIsolation: 'isolated'
+    },
+    externalClasses: ['custom-class']
+});
