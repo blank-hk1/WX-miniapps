722c646f0305acd50d697370fda0b09150893c5f
blank-hk1 <1577279487@qq.com>
2021-01-11 21:43:42 +0800

    Create user.js

---
+++ miniprogram-6/miniprogram/pages/user/user.js
@@ -0,0 +1,92 @@
+// pages/user/user.js
+var app=getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [
+      {name: 'male', value: '男孩'},
+      {name: 'female', value: '女孩', checked: 'true'},
+    ],
+    select:false,
+    education:'',
+    userAge:''
+  },
+  userAgeinput:function(e){
+   this.setData({
+    userAge:e.detail.value
+   })
+  },
+  bindShowMsg() {
+    this.setData({
+        select:!this.data.select
+    })
+},
+mySelect(e) {
+   var name = e.currentTarget.dataset.name
+   this.setData({
+    education: name,
+       select: false
+   })
+},
+  radioChange: function(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
